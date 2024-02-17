4e7d282dff4614f8b4b4ae2d87923e7b82c1c6e1
enzotorrs <enzotorres969@gmail.com>
2024-02-17 16:35:53 -0300

    add: asset model

---
+++ ceos/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from ceos.database import Base
+
+
+class Asset(Base):
+    __tablename__ = "assets"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    folder = Column(Boolean, default=True)
+    parent_asset_id = Column(Integer, ForeignKey("assets.id"))
+    child_assets = relationship("Asset")
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
