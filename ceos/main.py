d2449d034f1981b5ed201d611c1d68de586aa6c7
enzotorrs <enzotorres969@gmail.com>
2024-02-17 16:36:59 -0300

    add: asset read and create endpoints

--- ceos/main.py
+++ ceos/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from ceos import crud, schemas
 
 from .database import SessionLocal
 
@@ -13,6 +16,16 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, world"}
+@app.get("/assets/", response_model=list[schemas.Asset])
+def read_assets(db: Session = Depends(get_db)):
+    return crud.get_assets(db)
+
+
+@app.get("/assets/{asset_id}", response_model=schemas.Asset)
+def read_asset(asset_id: int, db: Session = Depends(get_db)):
+    return crud.get_asset(asset_id, db)
+
+
+@app.post("/assets/", response_model=schemas.Asset)
+def create_asset(asset: schemas.AssetCreate, db: Session = Depends(get_db)):
+    return crud.create_asset(db, asset)
