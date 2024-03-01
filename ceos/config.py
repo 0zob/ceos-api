44c3798cdc6f794bf9f54eddb65ea5731e018c87
enzotorrs <enzotorres969@gmail.com>
2024-03-01 16:10:25 -0300

    feat: add new field in config file

--- ceos/config.py
+++ ceos/config.py
@@ -6,6 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Config(BaseSettings):
     db_url: str = Field(alias="DB_URL")
+    root_path: str = Field(alias="ROOT_PATH")
     model_config = SettingsConfigDict(env_file=os.environ.get("ENV"))
 
 
