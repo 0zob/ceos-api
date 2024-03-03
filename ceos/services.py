c6a17911998ce0248ae41c8694759e60463c619a
enzotorrs <enzotorres969@gmail.com>
2024-03-03 11:47:06 -0300

    feat: add FileService for wrapper and replace file functions

--- ceos/services.py
+++ ceos/services.py
@@ -1,8 +1,16 @@
 from fastapi import HTTPException
+from ceos.config import config
+import os
 
 from ceos.schemas import AssetCreate, AssetUpdate
 
 
+class FileService:
+    def check_if_file_exists(self,file_path: str | None):
+        if not file_path:
+            return False
+        complete_path = os.path.join(config.root_path, file_path)
+        return os.path.isfile(complete_path)
 class UserService:
     def __init__(self, check_if_file_exists) -> None:
         self._file_exists = check_if_file_exists
