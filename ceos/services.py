6bf34ec2287fd9c97309416754f17b24418ec4a7
enzotorrs <enzotorres969@gmail.com>
2024-03-25 21:12:25 -0300

    refact AssetService for include db crud logic

--- ceos/services.py
+++ ceos/services.py
@@ -1,44 +1,67 @@
-import os
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
-from fastapi import Depends, HTTPException
-
-from ceos.config import config
 from ceos.schemas import AssetCreate, AssetPatch, AssetUpdate
 
+from . import models, schemas
 
-class UserService:
-    def validate_parent_asset(self, asset: AssetCreate, crud, db):
-        if not asset.parent_asset_id:
-            return
-        parent_asset = crud.get_asset(asset.parent_asset_id, db)
-        if not parent_asset or not parent_asset.folder:
-            raise HTTPException(
-                status_code=400,
-                detail="parent_asset_id target is not a folder or not exist",
-            )
 
-    def validate_parent_asset_for_update(
-        self, asset: AssetUpdate | AssetPatch, asset_id: int, crud, db
+class AssetService:
+    def get_assets(self, db: Session):
+        assets = db.query(models.Asset).all()
+        return assets
+
+    def get_asset(self, asset_id: int, db: Session):
+        return db.query(models.Asset).filter(models.Asset.id == asset_id).first()
+
+    def update_asset(
+        self,
+        db: Session,
+        asset_id: int,
+        asset: schemas.AssetUpdate | schemas.AssetPatch,
+        partial: bool = False,
     ):
-        stored_asset = crud.get_asset(asset_id, db)
+        stored_asset = self.get_asset(asset_id, db)
         if not stored_asset:
             raise HTTPException(
                 status_code=404, detail="The asset with this id does not exist"
             )
 
-        if not asset.parent_asset_id:
-            return stored_asset
-
-        if asset.parent_asset_id == asset_id:
+        if asset.parent_asset_id == stored_asset.id:
             raise HTTPException(
                 status_code=400,
                 detail="parent_asset_id cannot be the same as asset id",
             )
 
-        parent_asset = crud.get_asset(asset.parent_asset_id, db)
+        parent_asset = self.get_asset(asset.parent_asset_id, db)
         if not parent_asset or not parent_asset.folder:
             raise HTTPException(
                 status_code=400,
                 detail="parent_asset_id target is not a folder or not exist",
             )
+        db.query(models.Asset).filter(models.Asset.id == stored_asset.id).update(
+            asset.model_dump(exclude_none=partial)
+        )
+        db.commit()
+        db.refresh(stored_asset)
         return stored_asset
+
+    def create_asset(self, db: Session, asset: schemas.AssetCreate):
+        if asset.parent_asset_id:
+            parent_asset = self.get_asset(asset.parent_asset_id, db)
+            if not parent_asset or not parent_asset.folder:
+                raise HTTPException(
+                    status_code=400,
+                    detail="parent_asset_id target is not a folder or not exist",
+                )
+        db_asset = models.Asset(**asset.model_dump())
+        db.add(db_asset)
+        db.commit()
+        db.refresh(db_asset)
+        return db_asset
+
+    def delete_asset(self, asset_id: int, db: Session):
+        asset = db.query(models.Asset).get(asset_id)
+        db.delete(asset)
+        db.commit()
+        return asset
