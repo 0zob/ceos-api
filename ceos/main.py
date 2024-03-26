026e1e693645446e3d1a905c2ce1cbda7518f1ac
enzotorrs <enzotorres969@gmail.com>
2024-03-25 21:12:52 -0300

    refact: use AssetService in routes instead of crud

--- ceos/main.py
+++ ceos/main.py
@@ -2,8 +2,8 @@ from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
-from ceos import crud, schemas
-from ceos.services import UserService
+from ceos import schemas
+from ceos.services import AssetService
 
 from .database import SessionLocal
 
@@ -28,13 +28,20 @@ def get_db():
 
 
 @app.get("/assets/", response_model=list[schemas.Asset])
-def read_assets(db: Session = Depends(get_db)):
-    return crud.get_assets(db)
+def read_assets(
+    db: Session = Depends(get_db),
+    asset_service: AssetService = Depends(AssetService),
+):
+    return asset_service.get_assets(db)
 
 
 @app.get("/assets/{asset_id}", response_model=schemas.Asset)
-def read_asset(asset_id: int, db: Session = Depends(get_db)):
-    asset = crud.get_asset(asset_id, db)
+def read_asset(
+    asset_id: int,
+    db: Session = Depends(get_db),
+    asset_service: AssetService = Depends(AssetService),
+):
+    asset = asset_service.get_asset(asset_id, db)
     if not asset:
         raise HTTPException(status_code=404, detail="Asset not found")
     return asset
@@ -44,10 +51,9 @@ def read_asset(asset_id: int, db: Session = Depends(get_db)):
 def create_asset(
     asset: schemas.AssetCreate,
     db: Session = Depends(get_db),
-    user_service: UserService = Depends(UserService),
+    asset_service: AssetService = Depends(AssetService),
 ):
-    user_service.validate_parent_asset(asset, crud, db)
-    return crud.create_asset(db, asset)
+    return asset_service.create_asset(db, asset)
 
 
 @app.put("/assets/{asset_id}", response_model=schemas.Asset)
@@ -55,10 +61,9 @@ def update_asset(
     asset: schemas.AssetUpdate,
     asset_id: int,
     db: Session = Depends(get_db),
-    user_service: UserService = Depends(UserService),
+    asset_service: AssetService = Depends(AssetService),
 ):
-    stored_asset = user_service.validate_parent_asset_for_update(asset, asset_id, crud, db)
-    return crud.update_asset(db, stored_asset, asset)
+    return asset_service.update_asset(db, asset_id, asset)
 
 
 @app.patch("/assets/{asset_id}", response_model=schemas.Asset)
@@ -66,15 +71,14 @@ def partial_update_asset(
     asset: schemas.AssetPatch,
     asset_id: int,
     db: Session = Depends(get_db),
-    user_service: UserService = Depends(UserService),
+    asset_service: AssetService = Depends(AssetService),
 ):
-    stored_asset = user_service.validate_parent_asset_for_update(asset, asset_id, crud, db)
-    return crud.update_asset(db, stored_asset, asset, partial=True)
+    return asset_service.update_asset(db, asset_id, asset, partial=True)
 
 
 @app.delete("/assets/{asset_id}", response_model=schemas.Asset)
 def delete_asset(asset_id: int, db: Session = Depends(get_db)):
-    asset = crud.get_asset(asset_id, db)
+    asset = asset_service.get_asset(asset_id, db)
     if not asset:
         raise HTTPException(status_code=404, detail="Asset not found")
-    return crud.delete_asset(asset_id, db)
+    return asset_service.delete_asset(asset_id, db)
